1a780549aa9c8e5133e5ad189651e3fb880de4c4
larobbie <zayycev22@gmail.com>
2021-12-12 02:55:46 +0300

    fixed malloc

---
+++ get_next_line.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By:  <larobbie>                                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/23 19:12:23 by                   #+#    #+#             */
+/*   Updated: 2021/11/26 05:49:54 by                  ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 41
+# endif
+
+# include <stdlib.h>
+# include <fcntl.h>
+# include <unistd.h>
+
+size_t	ft_strlen(const char *c);
+char	*ft_strjoin(char *s1, char *s2);
+char	*get_next_line(int fd);
+int		n_not_in(char *s);
+int		dist(char *s);
+#endif
